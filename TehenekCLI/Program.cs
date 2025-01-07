58c7d618e4f1db032e64db08cf77e077cf5dce57
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-01-07 09:31:11 +0100

    Add project files.

---
+++ TehenekCLI/Program.cs
@@ -0,0 +1,27 @@
+﻿using TehenekCLI;
+
+const string FILEPATH = "D:\\PROJECTS\\CSHARP250107\\TehenekCLI\\src\\hozam.txt";
+
+List<Tehen> happyCows = [];
+
+using StreamReader sr = new(FILEPATH);
+
+while (!sr.EndOfStream)
+{
+    string adatSor = sr.ReadLine();
+
+    string id = adatSor.Split(';')[0];
+    string nap = adatSor.Split(';')[1];
+    string mennyiseg = adatSor.Split(';')[2];
+
+    Tehen aktTehen = new(id);
+    if(!happyCows.Contains(aktTehen))
+    {
+        happyCows.Add(aktTehen);
+    }
+    else
+    {
+        int index = happyCows.IndexOf(aktTehen);
+        happyCows[index].EredmenytRogzit(nap, mennyiseg);
+    }
+}
